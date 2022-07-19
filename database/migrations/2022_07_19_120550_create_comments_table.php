e9be4c23e1ec07e5f6ce16edb1bb56e319425b62
Andrey Lubnin <and2255@yandex.ru>
2022-07-19 15:55:05 +0300

    E54 final

--- database/migrations/2022_07_19_120550_create_comments_table.php
+++ database/migrations/2022_07_19_120550_create_comments_table.php
@@ -15,7 +15,14 @@ class CreateCommentsTable extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
+            // $table->unsignedBigInteger('post_id');
+            $table->foreignId('post_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            // $table->unsignedBigInteger('user_id');
+            $table->text('body');
             $table->timestamps();
+
+            // $table->foreign('post_id')->references('id')->on('posts')->cascadeOnDelete();
         });
     }
 
--- resources/views/components/post-comment.blade.php
+++ resources/views/components/post-comment.blade.php
@@ -1,18 +1,15 @@
 <article class="flex bg-gray-100 border border-gray-200 p-6 rounded-xl space-x-4">
     <div class="flex-shrink-0">
-        <img src="https://i.pravatar.cc/60" alt="" width="60" height="60" class="rounded-xl">
+        <img src="https://i.pravatar.cc/60?u={{ $comment->id }}" alt="" width="60" height="60" class="rounded-xl">
     </div>
     <div>
         <header class="mb-4">
-            <h3 class="font-bold">John Doe</h3>
+            <h3 class="font-bold">{{ $comment->author->username }}</h3>
             <p class="text-xs">
                 Posted
-                <time>8 months ago</time>
+                <time>{{ $comment->created_at }}</time>
             </p>
         </header>
-        <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Morbi blandit cursus risus at. Tincidunt augue interdum velit euismod in pellentesque massa. Ultricies lacus sed turpis tincidunt id aliquet risus. Tellus id interdum velit laoreet id donec ultrices tincidunt arcu. Egestas sed sed risus pretium. Risus commodo viverra maecenas accumsan. Magna ac placerat vestibulum lectus mauris ultrices. Neque gravida in fermentum et sollicitudin ac orci. Magna etiam tempor orci eu lobortis elementum nibh tellus molestie. Cum sociis natoque penatibus et. Vivamus at augue eget arcu dictum varius duis at consectetur. Dictum at tempor commodo ullamcorper a lacus vestibulum sed arcu. At consectetur lorem donec massa sapien faucibus et. Ac placerat vestibulum lectus mauris ultrices eros in. Commodo quis imperdiet massa tincidunt. Libero justo laoreet sit amet cursus. Netus et malesuada fames ac turpis egestas.
-        </p>
-
+        {{ $comment->body }}
     </div>
 </article>
